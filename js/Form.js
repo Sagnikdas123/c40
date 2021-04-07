5c01385dce5a1ab1ecef740f9b778f40e735d43b
Sagnikdas123 <sagnikred277@gmail.com>
2021-04-07 19:35:51 +0530

    added reset button

--- js/Form.js
+++ js/Form.js
@@ -5,6 +5,8 @@ class Form {
     this.button = createButton('Play');
     this.greeting = createElement('h2');
     this.title = createElement('h2');
+    this.reset = createButton('reset');
+
   }
   hide(){
     this.greeting.hide();
@@ -18,6 +20,7 @@ class Form {
     this.title.position(displayWidth/2 - 50, 0);
 
     this.input.position(displayWidth/2 - 40 , displayHeight/2 - 80);
+    this.reset.position(displayWidth-100,20);
     this.button.position(displayWidth/2 + 30, displayHeight/2);
 2
     this.button.mousePressed(()=>{
@@ -31,6 +34,9 @@ class Form {
       this.greeting.html("Hello " + player.name)
       this.greeting.position(displayWidth/2 - 70, displayHeight/4);
     });
-
+    this.reset.mousePressed(()=>{
+      player.updateCount(0);
+      game.update(0);
+    });
   }
 }
